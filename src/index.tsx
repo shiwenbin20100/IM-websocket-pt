cd5533ac25e7d153efca3685155071be444c06f6
eatWhatLunch <9706755@qq.com>
2018-04-16 23:51:05 +0800

    set up environment

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+
+
+class App extends React.Component {
+    render(){
+        return <h1>13235235231`4124</h1>
+    }
+}
+ReactDOM.render(
+    <App/>,
+    document.getElementById("exp")
+)
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname,"./assets")
+        // publicPath: "./assets/"
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts",".tsx",".js",".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader","css-loader"]
+            },
+            { 
+                enforce: "pre", test: /\.js$/, 
+                loader: "source-map-loader"
+             }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    devServer: {
+        host: "localhost",
+        port: 9000,
+        open: true,
+        hot: true
+    }
+}
